324d493623f6a0af2a09cb10502e6f2f0f4b8489
Mohit Agrawal <er.magrawal@gmail.com>
2015-12-05 10:11:09 +0530

    Update to config

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'newtruelancer'),
+            'database'  => env('DB_DATABASE', 'test'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'password'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => env('DB_PREFIX', ''),
